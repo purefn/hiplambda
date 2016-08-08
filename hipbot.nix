ef1fcdde9365c65df7a7ac70436bbef2bf865e9f
Brian McKenna <bmckenna@atlassian.com>
2016-08-08 21:41:20 +1000

    Update the fetching and hash of hipbot

--- hipbot.nix
+++ hipbot.nix
@@ -1,5 +1,5 @@
 { mkDerivation, aeson, base, bifunctors, blaze-builder, bytestring
-, either, exceptions, fetchgit, http-client, http-client-tls
+, either, exceptions, fetchFromGitHub, http-client, http-client-tls
 , http-types, jwt, lens, mtl, network-uri, postgresql-simple
 , resource-pool, safe, stdenv, stm, text, time, transformers
 , unordered-containers, utf8-string, wai, wai-lens, webcrank
@@ -8,8 +8,9 @@
 mkDerivation {
   pname = "hipbot";
   version = "0.6";
-  src = fetchgit {
-    url = "https://github.com/purefn/hipbot.git";
+  src = fetchFromGitHub {
+    owner = "purefn";
+    repo = "hipbot";
     sha256 = "10m1rnas92y0ykfrn75j9h6yl6gjsh7zjl4fmy6y9xxxyw4cynw7";
     rev = "a553a25e2aaf6ec2f1751b20cd40cbee839fb620";
   };
